3104cfb146c5a1b589e74d98c87cc1f8c7c1c0e3
Raghav Karthikeyan <raghavks99@gmail.com>
2023-05-13 15:52:33 +0530

    HREF values giving

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,13 +7,13 @@ function Navbar() {
 
             <nav className="nav nav-pills flex-column flex-sm-row bg-dark">
                 <NavLink to='/' className='Home'>
-                    <a className="flex-sm-fill text-sm-center nav-link">Home</a>
+                    <a className="flex-sm-fill text-sm-center nav-link" href='/'>Home</a>
                 </NavLink>
                 <NavLink to='/About' className='About'>
-                    <a className="flex-sm-fill text-sm-center nav-link">About</a>
+                    <a className="flex-sm-fill text-sm-center nav-link" href='/About'>About</a>
                 </NavLink>
                 <NavLink to='/Project' className='Projects'>
-                    <a className="flex-sm-fill text-sm-center nav-link">Projects</a>
+                    <a className="flex-sm-fill text-sm-center nav-link" href='/Project'>Projects</a>
                 </NavLink>
             </nav>
         </>
